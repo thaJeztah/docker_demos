42720b93913bfcda720aaaf360ee0ad166db45a2
Victor Vieux <vieux@docker.com>
2014-03-04 02:29:29 +0000

    enforce version

--- zero_downtime_hipache/stack.sh
+++ zero_downtime_hipache/stack.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #launch hipache
-docker run -d --name /hipache --expose 6379 -p 80:80 hipache
+docker run -d --name /hipache --expose 6379 -p 80:80 hipache:0.2.4
 sleep 1 
 
 #launch redis commander linked to redis server
 id_redis_comm=`docker run -d --name /redis-commander --expose 8081 --link /hipache:redis vieux/redis-commander`
 
 #launch registry
-id_registry=`docker run -d --name /registry --expose 5000 library/registry`
+id_registry=`docker run -d --name /registry --expose 5000 registry:0.6.5`
 
 #update hipache configuration
 ./set_hipache.sh $id_redis_comm redis.vieux.fr 8081 redis_comm
